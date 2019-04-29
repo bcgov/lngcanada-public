389f353bbd8ffd1cae4abf91e8103d65dd1284d5
Jamie Popkin <popkinj@littleearth.ca>
2019-04-29 13:33:15 -0700

    Project in header toggling

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,24 +1,27 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent {
-  constructor(public router: Router) {}
-
-  project = ((proj) => {
-    // console.log(this.router);
-    switch(proj){
-      case 1: return "LNG Canada";
-      case 2: return "Coastal Gas Link";
-      default: return "Projects"
-    }
-  })(1);
-
-  ngOnInit () {
-    console.log(this.router);
+  public project: string;
+  constructor(public router: Router) {
+    this.router.events.subscribe(ev => {
+      if (ev instanceof NavigationEnd) {
+        const r = /\/project\/(\d)\//;
+        const match = r.exec(location.pathname) || [];
+        this.project = ((proj) => {
+          switch(proj){
+            case '1': return "LNG Canada";
+            case '2': return "Coastal Gas Link";
+            default: return "Projects"
+          }
+        })(match[1]);
+      }
+    })
   }
 }
